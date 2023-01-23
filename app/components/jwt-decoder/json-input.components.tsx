958a0dcaa857535c76d1baf727e2ac955e41778a
Areeb Khan <areebkhan619@gmail.com>
2023-01-24 02:09:44 +0530

    Fixed the blur bug

--- app/components/jwt-decoder/json-input.components.tsx
+++ app/components/jwt-decoder/json-input.components.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import ReactCodeMirror, { ReactCodeMirrorProps } from '@uiw/react-codemirror';
+import React, { useRef, useState } from "react"
+import ReactCodeMirror, { ReactCodeMirrorProps, ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { json } from "@codemirror/lang-json"
 import { InputWrapper, JwtInputWrapper } from "./input.styles"
 import { EditorView } from "@codemirror/view";
@@ -25,8 +25,16 @@ const InputEditor: React.FC<IInputEditorProps> = ({ onValueChange, ...props }) =
 }
 
 export const JWTInputEditor: React.FC<IInputEditorProps> = ({ value, onValueChange, ...props }) => {
-  const [inFocus, setInFocus] = useState(false)
+  const [inFocus, setInFocus] = useState(false);
+  const inputRef = useRef<ReactCodeMirrorRef>(null);
   const subStrings = value.split(".")
+
+  const onChange = (value: string) => {
+    if (inputRef.current.view.hasFocus) {
+      onValueChange(value)
+    }
+  }
+
   return <JwtInputWrapper onMouseLeave={() => setInFocus(false)}
     onMouseEnter={() => setInFocus(true)} $inFocus={inFocus}>
     <ReactCodeMirror
@@ -36,16 +44,15 @@ export const JWTInputEditor: React.FC<IInputEditorProps> = ({ value, onValueChan
       extensions={[
         EditorView.lineWrapping
       ]}
-      onChange={(value, viewUpdate) => {
-        onValueChange(value)
-      }}
+      ref={inputRef}
+      onChange={onChange}
       minHeight="100%"
       className="code"
       {...props}
     >
       {
         !inFocus && <div onClick={() => setInFocus(true)} className="colored-token-parts">
-          {subStrings.map((str, idx) => <span className="str" key={str+idx}>{str}{idx < subStrings.length - 1 ? '.' : ''}</span>)}
+          {subStrings.map((str, idx) => <span className="str" key={str + idx}>{str}{idx < subStrings.length - 1 ? '.' : ''}</span>)}
         </div>
       }
 
