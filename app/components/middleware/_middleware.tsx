b6b974dd38972814678aadee79d4e5104930a34b
AreebKhan619 <areebkhan619@gmail.com>
2023-02-01 15:23:36 +0530

    Refactored the middleware file

--- app/components/middleware/_middleware.tsx
+++ app/components/middleware/_middleware.tsx
@@ -1,6 +1,8 @@
+// <root>/middleware.ts
+import type { NextRequest } from 'next/server'
 import { NextResponse } from "next/server"
 
-export function middleware(req) {
+export function middleware(req: NextRequest) {
     if (req.nextUrl.href.includes('/jwt-decoder/_next')) {
       return NextResponse.rewrite(
         req.nextUrl.href.replace('/jwt-decoder/_next', '/_next'),
