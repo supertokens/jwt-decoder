e5695c5a3eb47c1a4d3ff00464d950ff886a130b
AreebKhan619 <areebkhan619@gmail.com>
2023-02-03 02:33:07 +0530

    Removed commented code

--- app/next.config.js
+++ app/next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
     
   },
   basePath: '/jwt-decoder',
-  // assetPrefix: '/jwt-decoder',
-  // images: {
-  //   path: '/jwt-decoder/_next/image',
-  // },
   images: {
     unoptimized: true
   }
