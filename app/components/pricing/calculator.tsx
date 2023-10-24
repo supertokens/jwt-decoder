e4e1e01256c2709ec655a456a2357f6a8bdfc5ff
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-10-24 13:29:02 +0530

    add multitenancy pricing calculation UI

--- app/components/pricing/calculator.tsx
+++ app/components/pricing/calculator.tsx
@@ -12,18 +12,24 @@ export default function Calculator() {
     const [mau, setMAU] = useState(0);
 
     const [isMFAChecked, setIsMFAChecked] = useState(false);
-    const [dashboardUserCount, setDashboardUserCount] = useState(0);
     const [isDashboardUserChecked, setIsDashboardUserChecked] = useState(false);
     const [isAccountLinkingChecked, setIsAccountLinkingChecked] = useState(false);
     const [isMultitenancyChecked, setIsMultitenancyChecked] = useState(false);
+
+    const [dashboardUserCount, setDashboardUserCount] = useState(0);
+    const [enterpriseTenanatsCount, setEnterpriceTenantCount] = useState(0);
+    const [tenants0_3Count, setTenants0_3Count] = useState(0);
+    const [tenants3_10Count, setTenants3_10Count] = useState(0);
+    const [tenantsWith10PlusCount, setTenantsWith10PlusCount] = useState(0);
 
     const [generalMAUAmount, setGeneralMAUAmount] = useState(0);
     const [accountLinkingAmount, setAccountLinkingAmount] = useState(0);
     const [mfaAmount, setMFAAmount] = useState(0);
     const [dashboardAmount, setDashboardAmount] = useState(0);
-    const [tenants3_0, setTenants3_0] = useState(0);
-    const [tenants3_10, setTenants3_10] = useState(0);
-    const [tenanntsWith10PlusUsersAmount, setTenanntsWith10PlusUsersAmount] = useState(0);
+    const [enterpriseTenanatsAmount, setEnterpriceTenantAmount] = useState(0);
+    const [tenants0_3Amount, setTenants0_3Amount] = useState(0);
+    const [tenants3_10Amount, setTenants3_10Amount] = useState(0);
+    const [tenantsWith10PlusAmount, setTenantsWith10PlusAmount] = useState(0);
 
     const { activeTab } = usePricingToggleContext();
 
@@ -78,7 +84,7 @@ export default function Calculator() {
             if (mau >= 10000) {
                 if (isMFAChecked && isAccountLinkingChecked) {
                     setMFAAmount(mau * 0.01);
-                    setAccountLinkingAmount(mau * 0.005);
+                    setAccountLinkingAmount(mau * 0.01);
                 } else if (isMFAChecked) {
                     setMFAAmount(mau * 0.01);
                 } else if (isAccountLinkingChecked) {
@@ -119,7 +125,9 @@ export default function Calculator() {
                         {activeTab === "cloud" ? (
                             <>
                                 <h4 className={styles.margin_top_8}>${generalMAUAmount}</h4>
-                                <span>Free under 5K MAUs</span>
+                                <span>
+                                    Free under <span>5K</span> MAUs
+                                </span>
                             </>
                         ) : null}
                     </div>
@@ -132,10 +140,15 @@ export default function Calculator() {
                             }}
                         />
                         <span>MultiFactor Authentication</span>
-                        <span>Price / MAU: {activeTab === "cloud" ? "$0.005" : "$0.01"}</span>
+                        <span>
+                            Price / MAU: <span>{activeTab === "cloud" ? "$0.005" : "$0.01"}</span>
+                        </span>
                     </div>
                     <div className={styles.calculator__right__col}>
                         <h4>${mfaAmount}</h4>
+                        <span>
+                            Minimum <span>$100</span> / month
+                        </span>
                     </div>
                 </div>
                 <div className={styles.divider} />
@@ -147,10 +160,15 @@ export default function Calculator() {
                             }}
                         />
                         <span>Account Linking</span>
-                        <span>Price / MAU: {activeTab === "cloud" ? "$0.005" : "$0.01"}</span>
+                        <span>
+                            Price / MAU: <span>{activeTab === "cloud" ? "$0.005" : "$0.01"}</span>
+                        </span>
                     </div>
                     <div className={styles.calculator__right__col}>
                         <h4>${accountLinkingAmount}</h4>
+                        <span>
+                            Minimum <span>$100</span> / month
+                        </span>
                     </div>
                 </div>
                 <div className={styles.divider} />
@@ -167,55 +185,88 @@ export default function Calculator() {
                             value={dashboardUserCount}
                             onChange={e => setDashboardUserCount(Number(e.currentTarget.value))}
                         />
-                        <span>Price / User: $20</span>
+                        <span>
+                            Price / User: <span>$20</span>
+                        </span>
                     </div>
                     <div className={styles.calculator__right__col}>
                         <h4>${dashboardAmount}</h4>
+                        <span>
+                            Free for first <span>3</span> users
+                        </span>
                     </div>
                 </div>
                 <div className={styles.divider} />
                 <div className={styles.calculator__row}>
                     <div className={styles.calculator__left__col}>
-                        <CheckBox />
+                        <CheckBox onChange={(e)=> setIsMultitenancyChecked(e.currentTarget.checked)} />
                         <span>Multitenancy</span>
                     </div>
                     <div className={styles.calculator__right__col}>--</div>
                 </div>
-                <div className={styles.divider} />
+                {isMultitenancyChecked ? (
+                    <>
+                        <div className={styles.calculator__row}>
+                            <div className={`${styles.left__col__sub__row}`}>
+                                <span>Tenant with 0-3 users:</span>
+                                <Input
+                                    value={tenants0_3Count}
+                                    onChange={e => setTenants0_3Count(Number(e.currentTarget.value))}
+                                />
+                                <span>$2 /tenant / month | free for 25 tenants</span>
+                            </div>
+                            <div className={`${styles.right__col__sub__row}`}>
+                                <h4>${dashboardAmount}</h4>
+                            </div>
+                        </div>
+                        <div className={styles.calculator__row}>
+                            <div className={`${styles.left__col__sub__row}`}>
+                                <span>Tenant with 3-10 users:</span>
+                                <Input
+                                    value={tenants3_10Count}
+                                    onChange={e => setTenants3_10Count(Number(e.currentTarget.value))}
+                                />
+                                <span>$5 /tenant / month</span>
+                            </div>
+                            <div className={`${styles.right__col__sub__row}`}>
+                                <h4>${dashboardAmount}</h4>
+                            </div>
+                        </div>
+                        <div className={styles.calculator__row}>
+                            <div className={`${styles.left__col__sub__row}`}>
+                                <span>Tenant with 10+ users:</span>
+                                <Input
+                                    value={tenantsWith10PlusCount}
+                                    onChange={e => setTenantsWith10PlusCount(Number(e.currentTarget.value))}
+                                />
+                                <span>$10 /tenant / month</span>
+                            </div>
+                            <div className={`${styles.right__col__sub__row}`}>
+                                <h4>${dashboardAmount}</h4>
+                            </div>
+                        </div>
+                        <div className={styles.calculator__row}>
+                            <div className={`${styles.left__col__sub__row}`}>
+                                <span>Tenant with Enterprise SSO:</span>
+                                <Input
+                                    value={enterpriseTenanatsCount}
+                                    onChange={e => setEnterpriceTenantCount(Number(e.currentTarget.value))}
+                                />
+                                <span>$50 /tenant / month</span>
+                            </div>
+                            <div className={`${styles.right__col__sub__row}`}>
+                                <h4>${dashboardAmount}</h4>
+                            </div>
+                        </div>
+                    </>
+                ) : null}
                 <div className={styles.calculator__row}>
-                    <div className={styles.calculator__left__col}>
-                        <div>
-                            <span>Tenant with 0-3 users:</span>
-                            <Input value={tenants3_10} onChange={e => setTenants3_0(Number(e.currentTarget.value))} />
-                            <span>$2 /tenant / month | free for 25 tenants</span>
-                        </div>
-                        <div>
-                            <span>Tenant with 0-3 users:</span>
-                            <Input value={tenants3_10} onChange={e => setTenants3_10(Number(e.currentTarget.value))} />
-                            <span>$2 /tenant / month | free for 25 tenants</span>
-                        </div>
-                        <div>
-                            <span>Tenant with 0-3 users:</span>
-                            <Input value={tenants3_10} onChange={e => setTenants3_10(Number(e.currentTarget.value))} />
-                            <span>$2 /tenant / month | free for 25 tenants</span>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.calculator__row}>
-                    <div className={styles.calculator__left__col}>
-                        <span>Total</span>
-                    </div>
+                    <div className={styles.calculator__left__col}/>
                     <div className={styles.calculator__right__col}>
-                        <h4>
-                            $
-                            {Math.ceil(
-                                generalMAUAmount +
-                                    dashboardAmount +
-                                    tenanntsWith10PlusUsersAmount +
-                                    accountLinkingAmount +
-                                    mfaAmount
-                            )}
-                        </h4>
+                        <div className={styles.total__price__container}>
+                            <h2>${Math.ceil(generalMAUAmount + dashboardAmount + accountLinkingAmount + mfaAmount)}</h2>
+                            <span>Estimated Monthly Billing</span>
+                        </div>
                     </div>
                 </div>
             </div>
