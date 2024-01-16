08e72c622e387326d55b9183c644e621a705c4ed
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2024-01-16 11:55:31 +0530

    init product page.

---
+++ app/pages/product/index.tsx
@@ -0,0 +1,9 @@
+export default function ProductPage() {
+    return (
+        <main>
+            <section>
+                <div>hello world!</div>
+            </section>
+        </main>
+    );
+}
