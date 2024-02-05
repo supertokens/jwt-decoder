ceb5a8b42ad9e4152f79ccd0323bcb2b656b5b8d
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2024-02-05 12:05:13 +0530

    Remove bundle analyzer dependency

--- app/next.config.js
+++ app/next.config.js
@@ -8,12 +8,7 @@ const nextConfig = {
     basePath: "/jwt-encoder-decoder",
     images: {
         unoptimized: true
-    },
-    compress: true
+    }
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true"
-});
-
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
