aff08ac7701a850a9314536142115aaf042b8e5f
Favour Ohans <fohanekwu@gmail.com>
2024-10-26 09:17:37 +0100

    Arena and stacker hq case studies

---
+++ app/assets/images/customers/arena/index.ts
@@ -0,0 +1,11 @@
+import Logo from "./logo.png";
+import Cover from "./cover.png";
+import VartulAgrawal from "./vartul-agrawal.png";
+import MidCover from "./mid-cover.png";
+
+export const Arena = {
+    Logo,
+    Cover,
+    MidCover,
+    VartulAgrawal
+};
