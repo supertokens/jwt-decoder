6b66822e475ec85001de596c4887ea76e4bf16bc
Favour Ohans <fohanekwu@gmail.com>
2025-09-01 11:02:28 +0100

    Add n1co page

--- app/assets/images/customers/index.ts
+++ app/assets/images/customers/index.ts
@@ -3,6 +3,7 @@ import { Qloo } from "./qloo";
 import { Stacker } from "./stacker";
 import { Arena } from "./arena";
 import { Cocoon } from "./cocoon";
+import { N1co } from "./n1co";
 import QuoteStart from "./quote-start.png";
 import QuoteEnd from "./quote-end.png";
 import LinkedinIcon from "./linkedin.png";
@@ -14,6 +15,7 @@ export const CustomerAssets = {
     Stacker,
     Arena,
     Cocoon,
+    N1co,
     Common: {
         QuoteStart,
         QuoteEnd,
---
+++ app/assets/images/customers/n1co/index.ts
@@ -0,0 +1,11 @@
+import Logo from './logo.png';
+import Cover from './cover.png';
+import Christian from './christian.png';
+import MidCover from './mid-cover.png';
+
+export const N1co = {
+  Logo,
+  Cover,
+  MidCover,
+  Christian,
+};
